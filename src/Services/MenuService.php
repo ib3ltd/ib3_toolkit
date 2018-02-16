fea4993cca2008205136253f27561bd818a99fe1
Wayne Ashley <wayne@brand.rehab>
2018-02-16 14:22:46 +0000

    updated menu service for multi level menus

--- src/Services/MenuService.php
+++ src/Services/MenuService.php
@@ -9,11 +9,48 @@ use Drupal\ib3_toolkit\Interfaces\MenuInterface;
  */
 class MenuService implements MenuInterface {
 
+  private function resolveTree($menu_tree, $tree)
+  {
+    $entries = [];
+
+    foreach ($tree as $element) {
+
+      $link = $element->link;
+
+      $url = $link->getUrlObject();
+
+      $entry = [
+        'title' => $element->link->getTitle(),
+        'url' => null,
+        'state' => $element->inActiveTrail ? 'active' : '',
+        'children' => null,
+      ];
+
+      if ($url->isRouted()){
+        $entry['url'] = $url->toString();
+      } else {
+        $entry['url'] = $url->getUri();
+      }
+
+      if ($element->hasChildren) {
+        $entry['children'] = $this->resolveTree($menu_tree, $element->subtree);
+      }
+
+      $entries[] = $entry;
+    }
+
+    return $entries;
+  }
+
   public function getByName($menu_name)
   {
     $menu_tree = \Drupal::menuTree();
 
     $parameters = $menu_tree->getCurrentRouteMenuTreeParameters($menu_name);
+
+    $parameters->setMinDepth(0);
+    $parameters->setMaxDepth(20);
+
     $tree = $menu_tree->load($menu_name, $parameters);
 
     $manipulators = [
@@ -22,25 +59,7 @@ class MenuService implements MenuInterface {
     ];
 
     $tree = $menu_tree->transform($tree, $manipulators);
-    $menu = $menu_tree->build($tree);
-
-    $entries = [];
-
-    foreach ($menu['#items'] as $item) {
-
-      if ($item['url']->isRouted()) {
-        $alias = \Drupal::service('path.alias_storage')->load(['source' => '/node/'.$item['url']->getRouteParameters()['node']]);
-        $url = $alias['alias'];
-      } else {
-        $url = $item['url']->getUri();
-      }
-
-      $entries[] = [
-        'title' => $item['title'],
-        'url' => $url,
-        'state' => $item['in_active_trail'] ? 'active' : '',
-      ];
-    }
+    $entries = $this->resolveTree($menu_tree, $tree);
 
     return $entries;
   }
