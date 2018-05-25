15f99589a51308f41e97640bfc05dacee3e3f2c6
Wayne Ashley <wayne@brand.rehab>
2018-05-25 15:24:24 +0100

    added reference field trait

--- src/Traits/ReferenceField.php
+++ src/Traits/ReferenceField.php
@@ -32,6 +32,7 @@ trait ReferenceField {
   {
     extract($arr);
     $values = [];
+
     if (get_class($entity->get($parent_field_name)) == 'Drupal\Core\Field\EntityReferenceFieldItemList') {
       foreach($entity->get($parent_field_name) as $e) {
         $values[] = $e->entity;
@@ -41,5 +42,54 @@ trait ReferenceField {
       $e = $entity->get($parent_field_name);
       return $e->entity;
     }
+  }
+
+  protected function prepareParagraphEntityFromReferenceField($entity, $arr)
+  {
+    extract($arr);
+    $values = [];
+
+    if (get_class($entity->get($field_name)) == 'Drupal\Core\Field\EntityReferenceFieldItemList') {
+
+      foreach($entity->get($field_name) as $e) {
+        $uri = $e->entity->get($field_name)->entity->getFileUri();
+          $url = file_url_transform_relative(file_create_url($uri));
+            $values[] = [
+              'url' => $url,
+              'description' => array_key_exists('description', $e->getValue()) ? $e->getValue()['description'] : '',
+            ];
+      }
+      return $values;
+    } else {
+      $e = $entity->get($field_name);
+      return $e->entity;
+    }
+  }
+
+  protected function prepareFileFromParagraphReferenceField($entity, $arr)
+  {
+    extract($arr);
+    $paragraph = $this->fetchParagraph($entity, $paragraph_name);
+    if (is_array($paragraph)) {
+
+      $paragraphs = [];
+      $x = 0;
+      foreach ($paragraph as $para) {
+        if(is_array($para)) {
+          $paragraphs[$x] = [];
+          $y = 0;
+          foreach($para as $p) {
+            $paragraphs[$x][$y] = $p ? $this->prepareParagraphEntityFromReferenceField($p, ['field_name' => $field_name]) : null;
+            $y++;
+          }
+        } else {
+          $paragraphs[$x] = $para ? $this->prepareParagraphEntityFromReferenceField($para, ['parent_field_name' => $parent_field_name, 'field_name' => $field_name]) : null;
+        }
+        $x++;
+      }
+      return $paragraphs;
+    }
+    $paragraphs[0] = $paragraph ? $this->prepareParagraphEntityFromReferenceField($paragraph, ['field_name' => $field_name]) : null;
+    return $paragraphs;
   }
 }
